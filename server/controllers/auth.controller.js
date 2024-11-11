8615e568cb781b864f19d27462fc4a86afa1cf76
Mownica_webdev <mownica.p@gmail.com>
2024-11-12 01:15:39 +0530

    commenting  auth.ontroller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,12 +1,14 @@
 import User from "../models/User.model.js";
 import bcryptjs from "bcryptjs";
 export const signup = async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
+  const { username, email, password } = req.body; //Extracting User Data
+
+  const hashedPassword = bcryptjs.hashSync(password, 10); //Hashing the Password
+  const newUser = new User({ username, email, password: hashedPassword }); //Creating a New User Instance
+  //Saving the User in the Database
   try {
     await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    res.status(201).json({ message: "User created successfully" }); //Sending the Response
   } catch (err) {
     res.status(500).json(err.message);
   }
