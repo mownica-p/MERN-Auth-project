ebc74f7dbe1970f535a7610151ece44575e6a997
Mownica_webdev <mownica.p@gmail.com>
2024-11-11 23:18:02 +0530

    create test api

--- server/index.js
+++ server/index.js
@@ -15,3 +15,7 @@ const app = express();
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is working" });
+});
