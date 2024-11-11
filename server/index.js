51cd5cb64870e3f22b58936fa1c288ca4b9de3dd
Mownica_webdev <mownica.p@gmail.com>
2024-11-12 01:10:06 +0530

    installing bycrptjs for hashing password

--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
+//Import Statements
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth .route.js";
+
 import dotenv from "dotenv";
+
+//Environment Variable Configuration
 dotenv.config();
 
+//Database Connection
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+//Express Application Setup
 const app = express();
-
 app.use(express.json());
 
+//Server Listening
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
 
+//Route Handlers
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
