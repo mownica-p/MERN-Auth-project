80d1734f60c1d46927cde01a6121f75a1ae3899a
Mownica_webdev <mownica.p@gmail.com>
2024-11-12 00:49:10 +0530

    sending error if same user details are repeating

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,6 +3,10 @@ import User from "../models/User.model.js";
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
   const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json({ message: "User created successfully" });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 };
