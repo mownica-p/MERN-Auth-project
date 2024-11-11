e71a2827079c4efe5e9bd51722b41efaecaa241d
Mownica_webdev <mownica.p@gmail.com>
2024-11-11 17:28:32 +0530

    Create Header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <div className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold">Auth App</h1>
+        </Link>
+
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/signin">
+            <li>Signin</li>
+          </Link>
+          <Link to="/signup">
+            <li>Signup</li>
+          </Link>
+          <Link to="/profile">
+            <li>Profile</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
